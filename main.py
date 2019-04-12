bd669bbe9bd26c72fe3c5d79006e12cba2d4802d
xLeDocteurx <lenoirc.fr@gmail.com>
2019-04-12 09:24:50 +0200

    - Edited the git ignore - Refactored the functions as well as the error handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import walk
+import sys
 import re
 import time
 
@@ -29,14 +30,17 @@ def searchInFiles():
 	for fobj in files:
 		try:
 			filesCount = filesCount + 1
-			print("file:", filesCount, "/", len(files), "/", fobj.fullpath)
+			print("file:", filesCount, "/", len(files), "/", int(filesCount * 100 / len(files)), "%")
+			# sys.stdout.write("".join(["Progression :", str(int(filesCount * 100 / len(files))), "%"]))
+			# sys.stdout.flush()
+			# print("Progression :", int(filesCount * 100 / len(files)), "%", end="\r")
 			f = open(fobj.fullpath, "r")
 			# f = open(fobj.fullpath, "r",-1,"UTF-8")
 			if f.mode == "r":
-				print('\n'.join(re.findall('\\w*'+patternInput+'\\S*',f.read())))
-				# for line in re.findall('\\w*'+patternInput+'\\S*',f.read()):
-				# 	lines.append(ResultObject(fobj.fullpath, "x", line))
-				# if re.search(patternInput, line):
+				# print('\n'.join(re.findall('\\w*'+patternInput+'\\S*',f.read())))
+				for line in re.findall('\\w*'+patternInput+'\\S*',f.read()):
+					lines.append(ResultObject(fobj.fullpath, "x", line))
+					print(line)
 			f.close()
 		except:
 			errors.append(ErrorObject("searchInFiles Error", "An error occured while opening or reading the file" + fobj.fullpath))
@@ -75,10 +79,20 @@ print("-----------------------------------------------")
 # print(results, "lines in the list")
 endSearching = time.time()
 print("Searching execution time :", endSearching - startSearching)
+print("Number of registered lines : ", len(lines))
 print("-----------------------------------------------")
+
+printlines = input("Do you want to see a récap of the results résults ? ( enter 'yes' or 'y' to print them ) : ")
+if printlines == "y" or printlines == "yes":
+	for line in lines:
+		print(line.lineContent)
+	print("Number of registered lines : ", len(lines))
+	print("-----------------------------------------------")
 
 printErrors = input("Do you want to see the execution errors ? ( enter 'yes' or 'y' to print them ) : ")
 if printErrors == "y" or printErrors == "yes":
 	for error in errors:
 		print(error.errorType)
 		print(error.message)
+	print("Number of registered errors : ", len(errors))
+	print("-----------------------------------------------")
